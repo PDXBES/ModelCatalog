2e5df21837efb6b612e7e01b5306422999b2712b
Dan Ashney <daniel.ashney@portlandoregon.gov>
2018-06-26 14:29:54 -0700

    TDD setup

--- ModelCatalog.py
+++ ModelCatalog.py
@@ -1,14 +1,13 @@
+#TODO throw more specific exceptions
 
 class ModelCatalog:
     def __init__(self):
         self.models = []
 
     def add_model(self, model):
-        if model in self.models:
-            raise Exception
+        self.check_for_duplicate_model(model)
 
-        if not model.valid:
-            raise Exception
+        self.check_for_valid_model(model)
 
         self.models.append(model)
 
@@ -17,4 +16,24 @@ class ModelCatalog:
 
     def add_models(self, models):
         for model in models:
+            self.check_for_duplicate_model(model)
+        for model in models:
+            self.check_for_valid_model(model)
+
+        model_set = set()
+        for model in models:
+            if model not in model_set:
+                model_set.add(model)
+            else:
+                raise Exception
+
+        for model in models:
             self.add_model(model)
+
+    def check_for_duplicate_model(self, model):
+        if model in self.models:
+            raise Exception
+
+    def check_for_valid_model(self, model):
+        if not model.valid:
+            raise Exception
--- test_modelCatalog.py
+++ test_modelCatalog.py
@@ -11,7 +11,21 @@ class TestModelCatalog(TestCase):
         self.model1 = mock.MagicMock(Model)
         self.model2 = mock.MagicMock(Model)
 
+    def test_model_check_for_duplicates(self):
+        self.model1.valid = True
+        self.model_catalog.models.append(self.model1)
+        with self.assertRaises(Exception):
+            self.model_catalog.check_for_duplicate_model(self.model1)
+
+    def test_model_check_for_valid(self):
+        self.model1.valid = False
+        self.model_catalog.models.append(self.model1)
+        #self.model_catalog.check_for_valid_model(self.model1)
+        with self.assertRaises (Exception):
+            self.model_catalog.check_for_valid_model(self.model1)
+
     def test_add_model_new_model_into_empty_list(self):
+        self.model1.valid = True
         self.assertTrue(len(self.model_catalog.models) == 0)
 
         self.model_catalog.add_model(self.model1)
@@ -34,6 +48,11 @@ class TestModelCatalog(TestCase):
         with self.assertRaises(Exception):
             self.model_catalog.add_model(self.model1)
 
+    def test_add_model_duplicate_model_should_fail(self):
+        self.model1.valid = True
+        self.model_catalog.add_model(self.model1)
+        with self.assertRaises(Exception):
+            self.model_catalog.add_model(self.model1)
 
     def test_add_model_list_add_2_models_verify_2_models_in_catalog_models(self):
         models =[]
@@ -48,13 +67,50 @@ class TestModelCatalog(TestCase):
 
         self.assertTrue(self.model1 in self.model_catalog.models)
 
+    def test_add_model_list_1_model_valid_1_model_invalid(self):
+        models = []
 
+        self.model_catalog.models = []
 
-    def test_add_model_duplicate_model_should_fail(self):
+        self.model1.valid = True
+        self.model2.valid = False
 
-        self.model_catalog.add_model(self.model1)
+        models.append(self.model1)
+        models.append(self.model2)
+
         with self.assertRaises(Exception):
-            self.model_catalog.add_model(self.model1)
+            self.model_catalog.add_models(models)
+
+        self.assertEquals(len(self.model_catalog.models), 0)
+
+    def test_add_model_list_add_duplicate_model_and_check_no_models_added(self):
+        models = []
+
+        self.model_catalog.models.append(self.model1)
+
+        models.append(self.model1)
+
+        with self.assertRaises(Exception):
+            self.model_catalog.add_models(models)
+
+        self.assertEquals(len(self.model_catalog.models), 1)
+
+    def test_add_model_list_add_duplicate_model_to_empty_list_and_check_no_models_added(self):
+        models = []
+
+        self.model_catalog.models = []
+
+        self.model1.valid = True
+
+        models.append(self.model1)
+        models.append(self.model1)
+
+        with self.assertRaises(Exception):
+            self.model_catalog.add_models(models)
+
+        self.assertEquals(len(self.model_catalog.models), 0)
+
+
 
 
 
