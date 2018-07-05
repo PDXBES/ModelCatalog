5ce4f2eb7cc1408c4b84d67a9945a6be82498ee4
ROSE\SGOULD <samuel.gould@portlandoregon.gov>
2018-07-05 11:21:17 -0700

    Connected pyt GUI to business and dat io classes

--- dataIO.py
+++ dataIO.py
@@ -1,8 +1,14 @@
 import arcpy
+import os
 from modelCatalog_exception import ModelCatalog_exception, Field_names_length_does_not_match_row_length_exception
 
 class DataIO():
     def __init__(self):
+        connections = r"\\besfile1\ccsp\03_WP2_Planning_Support_Tools\03_RRAD\Model_Catalog\Dev\connection_files"
+        MODELCATALOG_sde = r"BESDBTEST1.MODELCATALOG.sde"
+        MODEL_CATALOG = os.path.join(connections, MODELCATALOG_sde)
+        self.ValueTable = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.VAT_MaxVal"
+        self.ModelTracking = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.ModelTracking"
         pass
 
     def retrieve_next_model_id(self, location, field_names):
@@ -30,36 +36,36 @@ class DataIO():
         cursor.insertRow(row)
         del cursor
 
-import os
-from Model import Model
-connections = r"\\besfile1\ccsp\03_WP2_Planning_Support_Tools\03_RRAD\Model_Catalog\Dev\connection_files"
-MODELCATALOG_sde = r"BESDBTEST1.MODELCATALOG.sde"
-MODEL_CATALOG = os.path.join(connections, MODELCATALOG_sde)
-ModelTracking = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.ModelTracking"
-ValueTable = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.VAT_MaxVal"
-ModelTracking = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.ModelTracking"
-dataio = DataIO()
-model = Model()
-current_id = dataio.retrieve_next_model_id(ValueTable, ["Object_Type", "Current_ID"])
-
-field_names = [
-    "Model_ID",
-    "Parent_Model_ID",
-    "Model_Request_ID",
-    "Project_Phase",
-    "Engine_Type",
-    "Create_Date",
-    "Deploy_Date",
-    "Run_Date",
-    "Model_Path",
-    "Project_Type",
-    "Model_Purpose",
-    "Model_Calibration_file",
-    "Model_Status",
-    "Model_Alterations",
-    "Model_Alteration_file",
-    "Project_Num"]
-
-model.Model_ID = current_id
-model.valid = True
-dataio.add_model(model,  ModelTracking, field_names)
+#import os
+# #from Model import Model
+# connections = r"\\besfile1\ccsp\03_WP2_Planning_Support_Tools\03_RRAD\Model_Catalog\Dev\connection_files"
+# MODELCATALOG_sde = r"BESDBTEST1.MODELCATALOG.sde"
+# MODEL_CATALOG = os.path.join(connections, MODELCATALOG_sde)
+# ModelTracking = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.ModelTracking"
+# ValueTable = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.VAT_MaxVal"
+# ModelTracking = MODEL_CATALOG + r"\MODEL_CATALOG.GIS.ModelTracking"
+# dataio = DataIO()
+# model = Model()
+# current_id = dataio.retrieve_next_model_id(ValueTable, ["Object_Type", "Current_ID"])
+#
+# field_names = [
+#     "Model_ID",
+#     "Parent_Model_ID",
+#     "Model_Request_ID",
+#     "Project_Phase",
+#     "Engine_Type",
+#     "Create_Date",
+#     "Deploy_Date",
+#     "Run_Date",
+#     "Model_Path",
+#     "Project_Type",
+#     "Model_Purpose",
+#     "Model_Calibration_file",
+#     "Model_Status",
+#     "Model_Alterations",
+#     "Model_Alteration_file",
+#     "Project_Num"]
+#
+# model.Model_ID = current_id
+# model.valid = True
+# dataio.add_model(model,  ModelTracking, field_names)
