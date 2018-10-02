fe2fae6a2d212fc7bbe20d145298a1ba80c37c11
Dan Ashney <daniel.ashney@portlandoregon.gov>
2018-10-02 13:39:47 -0700

    more

--- data_io.py
+++ data_io.py
@@ -1,4 +1,5 @@
 import arcpy
+from config import Config
 try:
     from typing import List, Any
 except:
@@ -6,11 +7,13 @@ except:
 
 from data_io_exception import DataIO_exception
 from data_io_exception import Field_names_length_does_not_match_row_length_exception
-class DataIO:
-    def __init__(self):
-        self.current_id_database_table_path = None
-        self.field_names = []
 
+class DataIO:
+    def __init__(self, config):
+        # type: (Config) -> None
+        self.current_id_database_table_path = None
+        self.field_attribute_lookup = None
+        self.config = config
 
     def retrieve_current_id(self, object_type):
         # type: (str, str) -> int
@@ -24,20 +27,26 @@ class DataIO:
         cursor.updateRow([object_name, next_id])
         return current_id
 
-    def map_object_attributes_to_database_fields(self, object_class):
-        pass
-
     def add_object(self, object_class):
 
         if not object_class.valid:
             raise DataIO_exception
 
-        row = self.map_object_attributes_to_database_fields()
+        row = self.create_row_from_object(object_class)
+        field_names = self.field_attribute_lookup.keys()
 
-        if len(self.field_names) != len(row):
+        if len(field_names) != len(row):
             raise Field_names_length_does_not_match_row_length_exception
 
-        cursor = arcpy.da.InsertCursor(self.config.model_tracking_sde_path, self.field_names)
+        cursor = arcpy.da.InsertCursor(self.config.model_tracking_sde_path, field_names)
 
         cursor.insertRow(row)
         del cursor
+
+    def create_row_from_object(self, generic_object):
+        attribute_names = self.field_attribute_lookup.values()
+        row = []
+        for attribute_name in attribute_names:
+            attribute_value = getattr(generic_object, attribute_name)
+            row.append(attribute_value)
+        return row
--- model_catalog_data_io.py
+++ model_catalog_data_io.py
@@ -7,13 +7,30 @@ except:
 from model import Model
 from config import Config
 from data_io import DataIO
+from collections import OrderedDict
 
 class ModelCatalogDataIO(DataIO):
     def __init__(self, config):
         # type: (Config) -> None
         self.config = config
         self.current_id_database_table_path = self.config.model_catalog_current_id_table_sde_path
-
+        self.field_attribute_lookup = OrderedDict()
+        self.field_attribute_lookup["Model_ID"] = "model_id"
+        self.field_attribute_lookup["Parent_Model_ID"] = "parent_model_id"
+        self.field_attribute_lookup["Model_Request_ID"] = "model_request_id"
+        self.field_attribute_lookup["Project_Phase_ID"] = "project_phase_id"
+        self.field_attribute_lookup["Engine_Type_ID"] = "engine_type_id"
+        self.field_attribute_lookup["Create_Date"] = "create_date"
+        self.field_attribute_lookup["Created_by"] = "created_by"
+        self.field_attribute_lookup["Deploy_Date"] = "deploy_date"
+        self.field_attribute_lookup["Extract_Date"] = "extract_date"
+        self.field_attribute_lookup["Run_Date"] = "run_date"
+        self.field_attribute_lookup["Model_Path"] = "model_path"
+        self.field_attribute_lookup["Model_Purpose_ID"] = "model_purpose_id"
+        self.field_attribute_lookup["Model_Calibration_file"] = "model_calibration_file"
+        self.field_attribute_lookup["Model_Status_ID"] = "model_status_id"
+        self.field_attribute_lookup["Model_Alteration_file"] = "model_alteration_file"
+        self.field_attribute_lookup["Project_Num"] = "project_num"
 
     def retrieve_current_model_id(self):
         current_model_id = self.retrieve_current_id("model")
@@ -23,34 +40,8 @@ class ModelCatalogDataIO(DataIO):
         current_simulation_id = self.retrieve_current_id("simulation")
         return current_simulation_id
 
-    def add_model(self, model, field_names):
-        # type: (Model, List[str]) -> None
-        if not model.valid:
-            raise ModelCatalog_exception
-
-        row = [model.model_id,
-               model.parent_model_id,
-               model.model_request_id,
-               model.project_phase_id,
-               model.engine_type_id,
-               model.create_date,
-               model.created_by,
-               model.deploy_date,
-               model.extract_date,
-               model.run_date,
-               model.model_path,
-               model.model_purpose_id,
-               model.model_calibration_file,
-               model.model_status_id,
-               model.model_alteration_file,
-               model.project_num]
-
-        if len(field_names) != len(row):
-            raise Field_names_length_does_not_match_row_length_exception
-
-        cursor = arcpy.da.InsertCursor(self.config.model_tracking_sde_path, field_names)
-
-        cursor.insertRow(row)
-        del cursor
+    def add_model(self, model):
+        # type: (Model) -> None
+        self.add_object(model)
 
 
