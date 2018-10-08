07485b4c445408817f85c9873d7882337616eeed
Brent Freeman <freembre@oregonstate.edu>
2018-10-08 11:51:37 -0700

    End of Sprint 6; Adjusted error handling of integration tests and .pyt; Minor updates to Documentation

--- emgaats_registration_integration_test.py
+++ emgaats_registration_integration_test.py
@@ -6,6 +6,7 @@ from model_data_io import ModelDataIO
 import getpass
 import datetime
 from config import Config
+from model_catalog_exception import Invalid_model_path_exception
 
 # This allows a file without a .py extension to be imported (ESRI pyt file)
 executable_path = os.path.dirname(os.path.realpath(__file__))
@@ -15,37 +16,41 @@ model_catalog_tools = load_source("ModelCatalog_tools", executable_path + "\\Mod
 ########################################################################################################################
 print "#####Testing working status model registration.#####"
 print "\n\n"
-config = Config()
-model = Model(config)
-model_dataio = ModelDataIO(config)
-model_catalog = ModelCatalog(config)
-model_catalog_dataio = ModelCatalogDataIO(config)
-model_id = model_catalog_dataio.retrieve_current_model_id()
-model.model_id = model_id
-model.parent_model_id = 555
-model.model_request_id = 777
-model.project_phase_id = 1
-model.engine_type_id = 1
-model.create_date = None
-model.deploy_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
-model.run_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
-model.extract_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
-model.created_by = getpass.getuser()
-model.model_path = r"\\BESFIle1\CCSP\03_WP2_Planning_Support_Tools\03_RRAD\Model_Catalog\Test_Cases\Carolina_Trunk\Base_Calib"
-# TODO: need 1-M for project types, write code to add project types
-# TODO: need 1-M for model alterations, write code to add model alterations
-model.project_type_id = 1
-model.model_purpose_id = config.model_purpose_id["Calibration"]
-model.model_calibration_file = "C:\Temp\Cal"
-model.model_status_id = config.model_status_id["Working"]
-model.model_alteration_file = "C:\Temp\BC"
-model.project_num = "E10TEST"
-model.valid
-model_dataio.create_model_geometry(model)
-model.create_date = datetime.datetime.today()
+try:
+    config = Config()
+    model = Model(config)
+    model_dataio = ModelDataIO(config)
+    model_catalog = ModelCatalog(config)
+    model_catalog_dataio = ModelCatalogDataIO(config)
+    model_id = model_catalog_dataio.retrieve_current_model_id()
+    model.model_id = model_id
+    model.parent_model_id = 555
+    model.model_request_id = 777
+    model.project_phase_id = 1
+    model.engine_type_id = 1
+    model.create_date = None
+    model.deploy_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
+    model.run_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
+    model.extract_date = None  # TODO NEEDS TO BE EXTRACTED FROM CONFIG FILE
+    model.created_by = getpass.getuser()
+    model.model_path = r"\\BESFIle1\CCSP\03_WP2_Planning_Support_Tools\03_RRAD\Model_Catalog\Test_Cases\Carolina_Trunk\Base_Calib"
+    # TODO: need 1-M for project types, write code to add project types
+    # TODO: need 1-M for model alterations, write code to add model alterations
+    model.project_type_id = 1
+    model.model_purpose_id = config.model_purpose_id["Calibration"]
+    model.model_calibration_file = "C:\Temp\Cal"
+    model.model_status_id = config.model_status_id["Working"]
+    model.model_alteration_file = "C:\Temp\BC"
+    model.project_num = "E10TEST"
+    model.valid
+    model_dataio.create_model_geometry(model)
+    model.create_date = datetime.datetime.today()
 
-model_catalog.add_model(model)
-model_catalog_tools.EMGAATS_Model_Registration_function(model_catalog, config)
+    model_catalog.add_model(model)
+    model_catalog_tools.EMGAATS_Model_Registration_function(model_catalog, config)
+except Invalid_model_path_exception:
+    print("Current Model Path does not point to a valid EMGAATS model")
+    quit()
 print "\n\n"
 
 ########################################################################################################################
@@ -74,5 +79,6 @@ try:
     model.model_path = r"Invalid_path"
     model_catalog.add_model(model)
     model_catalog_tools.EMGAATS_Model_Registration_function(model_catalog, config)
-except:
+except Invalid_model_path_exception:
     print("Current Model Path does not point to a valid EMGAATS model")
+    quit()
